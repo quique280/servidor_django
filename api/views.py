6387715f3045e4ef2a0e9989a40f3b5c6888c09c
Unknown <quique98_diaz@hotmail.com>
2018-11-21 01:43:14 -0600

    Commit servidor luis

--- api/views.py
+++ api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from pruebas.models import Prueba
 from pruebas.models import Deduccion
@@ -11,9 +12,50 @@ from .serializers import PruebaSerializer
 from .serializers import DeduccionSerializer
 from .serializers import InferenciaSerializer
 
+from api.parser.grammar.WangLexer import WangLexer
+from api.parser.grammar.WangParser import WangParser
+from api.src.visitor import WangPrintVisitor, WangCreateDeductionVisitor
+from api.src.wang import MyErrorListener
+from pruebas.src.proof import Probador
+
+from antlr4 import *
+from antlr4.error.ErrorListener import ErrorListener, ConsoleErrorListener
+
+from django.db.models import DateTimeField
+import datetime
 class PruebaAPIView(generics.ListCreateAPIView):
     queryset = Prueba.objects.all()
     serializer_class = PruebaSerializer
+
+    def post(self, request):
+        if 'afirmacion' in request.data:
+            afir = request.data.pop('afirmacion')
+            lexer = WangLexer(InputStream(afir))
+            token_stream = CommonTokenStream(lexer)
+            parser = WangParser(token_stream)
+            parser.removeErrorListeners()
+            parser.addErrorListener(MyErrorListener())
+            try:
+                tree = parser.deduction()
+            except SyntaxError as e: #TODO: Manejar mejor la excepcion
+                print(e.msg)
+                sys.exit(-1)
+            visitor = WangCreateDeductionVisitor()
+            ded = visitor.visit(tree)
+            pr = Probador(ded)
+            if not 'fecha' in request.data:
+                fec = datetime.datetime.now()
+            prub = {
+                'afirmacion': str(pr.afirmacion),
+                'fecha': fec,
+                'inferencias': pr.inferencias,
+                'deducciones': pr.deducciones
+            }
+            serializer = PruebaSerializer(data=prub)
+            if(serializer.is_valid()):
+                serializer.create(prub)
+                return Response(serializer.initial_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PruebaAPIDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Prueba.objects.all()
--- pruebas/src/proof.py
+++ pruebas/src/proof.py
@@ -16,6 +16,7 @@ from enum import Enum, auto
 from abc import ABCMeta, abstractmethod
 from pruebas.src.utils import *
 from pruebas.src.tree import Tree
+from pruebas.models import *
 
 class RuleType(Enum):
     AXIOM = auto()
@@ -44,14 +45,20 @@ class Rule(metaclass=ABCMeta):
     def kind(self):
         return self.__ruletype
 
+    def __call__(self, deduction):
+        return self.apply(deduction)
+
 class Axiom(Rule):
     def __init__(self):
         super().__init__(RuleType.AXIOM)
     
     def apply(self, deduction):
-        yield from iFirst(((self.kind, (x[0], y[0]), deduction) 
+        yield from iFirst(((self, (x[0], y[0]), deduction) 
         for x in enumerate(deduction.left)
         for y in enumerate(deduction.right) if x[1] == y[1]))
+
+    def __str__(self):
+        return "Axiom"
         
 
 AXIOM_RULE = Axiom()
@@ -61,9 +68,12 @@ class AndLeft(Rule):
         super().__init__(RuleType.AND_LEFT)
     def apply(self, deduction):
         ands = (f for f in enumerate(deduction.left) if isinstance(f[1], And))
-        yield from iFirst(((self.kind, (f[0], None),
+        yield from iFirst(((self, (f[0], None),
             Deduction(list(replace(deduction.left, f[0], [f[1].left, f[1].right])), deduction.right))
             for f in ands))
+
+    def __str__(self):
+        return "And-Left"
             
 AND_LEFT_RULE= AndLeft()        
             
@@ -72,9 +82,12 @@ class OrRight(Rule):
         super().__init__(RuleType.OR_RIGHT)
     def apply(self, deduction):
         ors = (f for f in enumerate(deduction.right) if isinstance(f[1], Or))
-        yield from iFirst(((self.kind, (None, f[0]),
+        yield from iFirst(((self, (None, f[0]),
             Deduction(deduction.left, list(replace(deduction.right, f[0], [f[1].left, f[1].right]))))
             for f in ors))
+
+    def __str__(self):
+        return "Or-Right"
 
 OR_RIGHT_RULE= OrRight()
 
@@ -89,8 +102,11 @@ class OrLeft(Rule):
         if(firstOr != None):
             replaced1 = list(replace(deduction.left, firstOr[0], [firstOr[1].left]))
             replaced2 = list(replace(deduction.left, firstOr[0], [firstOr[1].right]))
-            yield (self.kind, (firstOr[0], None), Deduction(replaced1, deduction.right))
-            yield (self.kind, (firstOr[0], None), Deduction(replaced2, deduction.right))
+            yield (self, (firstOr[0], None), Deduction(replaced1, deduction.right))
+            yield (self, (firstOr[0], None), Deduction(replaced2, deduction.right))
+
+    def __str__(self):
+        return "Or-Left"
             
 OR_LEFT_RULE= OrLeft()  
 
@@ -104,8 +120,11 @@ class AndRight(Rule):
         if(firstAnd != None):
             replaced1 = list(replace(deduction.right, firstAnd[0], [firstAnd[1].left]))
             replaced2 = list(replace(deduction.right, firstAnd[0], [firstAnd[1].right]))
-            yield (self.kind, (None, firstAnd[0]), Deduction(deduction.left, replaced1))
-            yield (self.kind, (None, firstAnd[0]), Deduction(deduction.left, replaced2))
+            yield (self, (None, firstAnd[0]), Deduction(deduction.left, replaced1))
+            yield (self, (None, firstAnd[0]), Deduction(deduction.left, replaced2))
+
+    def __str__(self):
+        return "And-Right"
             
 AND_RIGHT_RULE= AndRight()  
 
@@ -114,9 +133,12 @@ class EquivImpliesLeft(Rule):
         super().__init__(RuleType.EQUIV_IMPLIES_LEFT)
     def apply(self, deduction):
         thens = (f for f in enumerate(deduction.left) if isinstance(f[1], Then))
-        return iFirst(((self.kind, (f[0], None), 
+        return iFirst(((self, (f[0], None), 
             Deduction(list(replace(deduction.left, f[0], [f[1].weak_nf()])), deduction.right)) 
             for f in thens))
+
+    def __str__(self):
+        return "Equiv.Implies-Left"
             
 EQUIV_IMPLIES_LEFT = EquivImpliesLeft() 
 
@@ -125,9 +147,12 @@ class EquivImpliesRight(Rule):
         super().__init__(RuleType.EQUIV_IMPLIES_RIGHT)
     def apply(self, deduction):
         thens = (f for f in enumerate(deduction.right) if isinstance(f[1], Then))
-        return iFirst(((self.kind, (None, f[0]), 
+        return iFirst(((self, (None, f[0]), 
             Deduction(deduction.left, list(replace(deduction.right, f[0], [f[1].weak_nf()])))) 
             for f in thens))
+
+    def __str__(self):
+        return "Equiv.Implies-Right"
             
 EQUIV_IMPLIES_RIGHT = EquivImpliesRight() 
 
@@ -136,9 +161,12 @@ class EquivBicondLeft(Rule):
         super().__init__(RuleType.EQUIV_BICOND_LEFT)
     def apply(self, deduction):
         biconds = (f for f in enumerate(deduction.left) if isinstance(f[1], Bicond))
-        return iFirst(((self.kind, (f[0], None), 
+        return iFirst(((self, (f[0], None), 
             Deduction(list(replace(deduction.left, f[0], [f[1].weak_nf()])), deduction.right)) 
             for f in biconds))
+
+    def __str__(self):
+        return "Equiv.Bicond-Left"
             
 EQUIV_BICOND_LEFT = EquivBicondLeft() 
 
@@ -147,9 +175,12 @@ class EquivBicondRight(Rule):
         super().__init__(RuleType.EQUIV_BICOND_RIGHT)
     def apply(self, deduction):
         biconds = (f for f in enumerate(deduction.right) if isinstance(f[1], Bicond))
-        return iFirst(((self.kind, (None, f[0]), 
+        return iFirst(((self, (None, f[0]), 
             Deduction(deduction.left, list(replace(deduction.right, f[0], [f[1].weak_nf()])))) 
             for f in biconds))
+
+    def __str__(self):
+        return "Equiv.Bicond-Right"
             
 EQUIV_BICOND_RIGHT = EquivBicondRight()  
 
@@ -161,8 +192,11 @@ class NotLeft(Rule):
         for f in nots:
             newLeft, newRight = deduction.left[:], deduction.right[:]
             newRight.insert(0, newLeft.pop(f[0]).left)
-            yield (self.kind, (f[0], None), Deduction(newLeft, newRight))
+            yield (self, (f[0], None), Deduction(newLeft, newRight))
             return
+
+    def __str__(self):
+        return "Not-Left"
 
 NOT_LEFT = NotLeft()  
 
@@ -174,8 +208,11 @@ class NotRight(Rule):
         for f in nots:
             newLeft, newRight = deduction.left[:], deduction.right[:]
             newLeft.insert(0, newRight.pop(f[0]).left)
-            yield (self.kind, (None, f[0]), Deduction(newLeft, newRight))
+            yield (self, (None, f[0]), Deduction(newLeft, newRight))
             return
+
+    def __str__(self):
+        return "Not-Right"
 
 NOT_RIGHT = NotRight()  
         
@@ -189,42 +226,50 @@ class Probador():
     def deducciones(self):
         if(not self.__deducciones):
             self.probar()
-        return __deducciones
+        return self.__deducciones
 
     @property
     def inferencias(self):
         if(not self.__inferencias):
-            self.probar()
-        return __inferencias
+            self.__inferencias, self.__deducciones = self.inferenciasDeducciones(self.arbolPrueba)
+        return self.__inferencias
 
     @property
     def arbolPrueba(self):
         if(not self.__arbolPrueba):
-            self.__arbolPrueba = Tree(('INITIAL_STATE', None, self.afirmacion))
-            for ar in self.probar():
-                self.__arbolPrueba.addChild(ar)
+            self.__arbolPrueba = Tree(('Initial State', None, self.afirmacion))
+            list(map(lambda x:self.__arbolPrueba.addChild(x),self.probar()))
         return self.__arbolPrueba
 
-    def applySingleChildRule(self, deduct, rule):
-        applied = first(rule.apply(deduct))
-        if(applied != None):
-            arbol = Tree(andLeftApplied)
-            for ar in self.probar(andLeftApplied[2]):
-                arbol.addChild(ar)
-            yield arbol
-            return
-        
+    def inferenciasDeducciones(self, arbol):
+        inferencias = []
+        deducciones = []
+        for ch in arbol.children:
+            inferenciasHijas, deduccionesHijas = self.inferenciasDeducciones(ch)
+            inferencias += inferenciasHijas
+            deducciones += deduccionesHijas
+            deducciones.append({
+                "afirmacion": str(arbol.value[2])
+            })
+            inferencias.append({
+                'deduccionVieja': str(arbol.value[2]),
+                'deduccionNueva': str(ch.value[2]),
+                'regla': str(ch.value[0]),
+                'reglaPosIzq': ch.value[1][0] if ch.value[1] else None,
+                'reglaPosDer': ch.value[1][1] if ch.value[1] else None
+            })
+        return inferencias, deducciones
+
     def chainApply(self, deduct, iterRules):
-        applied = firstRuleAppliedNotNone(deduct, iterRules)
+        applied = firstAppliedNotNone(deduct, iterRules)
         if(applied != None):
             for ch in applied:
                 arbol = Tree(ch)
-                if ch[0] != RuleType.AXIOM:
-                    for ar in self.chainApply(ch[2], iterRules):
-                        arbol.addChild(ar)
+                if ch[0].kind != RuleType.AXIOM:
+                    list(map(lambda x:arbol.addChild(x),self.chainApply(ch[2], iterRules)))
                 yield arbol
             return
-        yield Tree(('FAILURE', None, deduct))
+        yield Tree(('Unable to prove', None, deduct))
 
     
     def probar(self):
@@ -240,19 +285,9 @@ class Probador():
             EQUIV_BICOND_RIGHT,
             NOT_LEFT,
             NOT_RIGHT,
-            ]
+        ]
         yield from self.chainApply(self.afirmacion, ruleList)
         
-
-def probar(deduction, hijos=[]):
-    axioma = first(AXIOM_RULE.apply(deduction))
-    if(axioma != None):
-        return  [axioma]
-    aux = first(AND_LEFT_RULE.apply(deduction))
-    if(aux != None):
-        hijos += aux
-        hijos += probar(aux[2])
-    return hijos
 
 if __name__ == "__main__":
     print("*** Testing Proofs ***")
@@ -266,10 +301,8 @@ if __name__ == "__main__":
     c = Then(p, b)
     ded = Deduction([a, b, p], [na, c, p])
     print("1) Axiom test", ded)
-    for f in AXIOM_RULE.apply(ded):
-        print(f)
+    list(map(lambda x: print(x),AXIOM_RULE.apply(ded)))
     print("2) AndLeft Test", ded)
-    for f in AND_LEFT_RULE.apply(ded):
-        print(f)
+    list(map(lambda x: print(x),AND_LEFT_RULE.apply(ded)))
     
     
